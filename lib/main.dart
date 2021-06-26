2b2835a8176a5f85d43b3f75d4dd7489f62729b5
darshannere <73795559+darshannere@users.noreply.github.com>
2021-06-26 11:44:30 +0530

    Added reset level

--- lib/main.dart
+++ lib/main.dart
@@ -2,42 +2,44 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home:ninjacard() ,
+    home: ninjacard(),
   ));
 }
+
 class ninjacard extends StatefulWidget {
   @override
   _ninjacardState createState() => _ninjacardState();
 }
 
 class _ninjacardState extends State<ninjacard> {
-  int ninjalevel=0;
+  int ninjalevel = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           setState(() {
-            ninjalevel+=1;
+            ninjalevel += 1;
           });
         },
-        child: Icon(Icons.add) ,
-        backgroundColor:Colors.grey[800] ,),
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
+      ),
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title: Text("ninja id card"),
         centerTitle: true,
         backgroundColor: Colors.grey[850],
-
       ),
       body: Padding(
-        padding: EdgeInsets.fromLTRB(30,40,30,0),
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Center(
               child: CircleAvatar(
-                backgroundImage: NetworkImage('https://www.woolha.com/media/2020/03/eevee.png'),
+                backgroundImage: NetworkImage(
+                    'https://www.woolha.com/media/2020/03/eevee.png'),
                 backgroundColor: Colors.transparent,
                 radius: 35,
               ),
@@ -46,71 +48,74 @@ class _ninjacardState extends State<ninjacard> {
               height: 60,
               color: Colors.grey[800],
             ),
-            Text("NAME",
+            Text(
+              "NAME",
               style: TextStyle(
                 color: Colors.grey,
                 letterSpacing: 2,
-
               ),
             ),
-            SizedBox(height:10),
-            Text("Darshan Nere",
+            SizedBox(height: 10),
+            Text(
+              "Darshan Nere",
               style: TextStyle(
                 color: Colors.amber[200],
                 letterSpacing: 2,
                 fontSize: 28,
                 fontWeight: FontWeight.bold,
-
               ),
             ),
-            SizedBox(height:30),
-            Text("Current ninja level",
+            SizedBox(height: 30),
+            Text(
+              "Current ninja level",
               style: TextStyle(
                 color: Colors.grey,
                 letterSpacing: 2,
-
               ),
             ),
-            SizedBox(height:10),
-            Text('$ninjalevel',
+            SizedBox(height: 10),
+            Text(
+              '$ninjalevel',
               style: TextStyle(
                 color: Colors.amber[200],
                 letterSpacing: 2,
                 fontSize: 28,
                 fontWeight: FontWeight.bold,
-
               ),
             ),
-            SizedBox(height:30),
+            SizedBox(height: 30),
             Row(
               children: [
-                Icon(Icons.email,
+                Icon(
+                  Icons.email,
                   color: Colors.grey,
-
-
                 ),
-                SizedBox(width: 10,),
-                Text("darshan@nere.com",
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "darshan.nere@gmail.com",
                   style: TextStyle(
                     color: Colors.grey,
                     fontSize: 18,
                     letterSpacing: 1,
-
-
                   ),
-
-
                 ),
               ],
             ),
-
+            TextButton(
+                onPressed: () {
+                  setState(() {
+                    ninjalevel = 0;
+                  });
+                },
+                child: Text(
+                  "Reset level",
+                  style: TextStyle(color: Colors.grey, fontSize: 18),
+                ))
           ],
         ),
       ),
     );
   }
 }
-
-
-
-
